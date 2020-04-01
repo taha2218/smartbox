6bf1cdcc13e96789a24faca120d13121f711bd66
taha2218 <ta.22.ha.18.s@gmail.com>
2020-04-02 04:57:57 +0530

    Updates

---
+++ precache-manifest.f9b108b23948cf740624b1d734446079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909d30df8183f9692f37dcc307108a02",
+    "url": "/smartbox/index.html"
+  },
+  {
+    "revision": "af9831a1ed6a9e95bed4",
+    "url": "/smartbox/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "82d7be078acfcae81119",
+    "url": "/smartbox/static/css/main.c218cce4.chunk.css"
+  },
+  {
+    "revision": "af9831a1ed6a9e95bed4",
+    "url": "/smartbox/static/js/2.a690048a.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/smartbox/static/js/2.a690048a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d7be078acfcae81119",
+    "url": "/smartbox/static/js/main.e5962083.chunk.js"
+  },
+  {
+    "revision": "27cda607e54a93f49a81",
+    "url": "/smartbox/static/js/runtime-main.53f4a7a9.js"
+  }
+]);
